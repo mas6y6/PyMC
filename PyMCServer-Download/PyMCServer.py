2242c17ca62a34d767d7406ee1bcdd534757e981
BuildTools <unconfigured@null.spigotmc.org>
2023-07-26 16:50:32 -0500

    Updated to v0.2-alpha

--- PyMCServer-Download/PyMCServer.py
+++ PyMCServer-Download/PyMCServer.py
@@ -111,7 +111,7 @@ def question(text,options,desc="",returnitemname=False):
     else:
         raise TypeError("returnitemname accepts Boolean values")
 
-def inputquestion(text,desc):
+def inputquestion(text,desc=""):
     run("clear")
     print(f"{Back.WHITE}{Fore.BLACK}{text}{Fore.RESET}{Back.RESET}")
     print()
@@ -120,10 +120,28 @@ def inputquestion(text,desc):
 
 def menu(text,options,desc="",returnitemname=False):
     select = 0
+    if settings["color"] == 1:
+        colord = Back.RESET
+    elif settings["color"] == 2:
+        colord = Back.BLACK
+    elif settings["color"] == 3:
+        colord = Back.RED
+    elif settings["color"] == 4:
+        colord = Back.BLUE
+    elif settings["color"] == 5:
+        colord = Back.GREEN
+    elif settings["color"] == 6:
+        colord = Back.CYAN
+    elif settings["color"] == 7:
+        colord = Back.MAGENTA
+    elif settings["color"] == 8:
+        colord = Back.YELLOW
+    elif settings["color"] == 9:
+        colord = Back.WHITE
     while True:
         run("clear")
         maxlen = len(options)
-        print(f"{Back.CYAN}{Fore.BLACK}{text}{Fore.RESET}{Back.RESET}".center(20,"#"))
+        print(f"{colord}{Fore.BLACK}{text}{Fore.RESET}{Back.RESET}".center(20,"#"))
         print()
         print(desc)
         print()
@@ -149,20 +167,42 @@ def menu(text,options,desc="",returnitemname=False):
     else:
         raise TypeError("returnitemname accepts Boolean values")
 
-def loading():
-    lu = progressbar.ProgressBar(widgets=[Fore.RESET,progressbar.AnimatedMarker()])
-    while True:
-        lu.update(0)
-
-def startserver():
-    cmd = Popen(f"sh start.sh",shell=True,stdout=PIPE)
+def startserver(showserver=False):
+    if showserver:
+        cmd = Popen(f"sh start.sh",shell=True)
+    else:
+        cmd = Popen(f"sh start.sh",shell=True,stdout=PIPE)
     output, err = cmd.communicate()
     return [output.decode(),err]
 
 def check_for_updates():
-    data = requests.get("https://api.github.com/repos/mas6y6/PyMC-Server/releases")
-    returndata = list(json.loads(data.text))
-    return returndata[len(returndata) - 1]["tag_name"]
+    info("Requesting https://apt.github.com/")
+    try:
+        data = requests.get("https://api.github.com/repos/mas6y6/PyMC-Server/releases",timeout=10)
+    except requests.exceptions.Timeout:
+        error("Server failed to respond. Please check your network. Failure_name: TIMEOUT")
+        data = None
+        return data
+    except requests.exceptions.ConnectionError as err:
+        error(f"Unable to connect to github. Check your network connection. full error below:\n{err}")
+        data = None
+        return data
+
+    try:
+        returndata = list(json.loads(data.text))
+    except:
+        error("Failed to compile versions")
+        data = None
+        return data
+
+    try:
+        data = returndata[len(returndata) - 1]["tag_name"]
+    except:
+        error("Github REST API request limit has been reached unable to get updates")
+        data = None
+        return data
+
+    return data
 
 def setup():
     dirs = [item for item in os.listdir() if not item.startswith(".")]
@@ -177,6 +217,7 @@ def setup():
         f.close()
     ok("Success")
     time.sleep(0.5)
+    servername = inputquestion("What the name of your NEW minecraft server")
     while True:
         directory = ""
         q = question("Where do you want to install your Minecraft server",dirs,desc="Use arrow keys to navigate",returnitemname=True)
@@ -322,9 +363,9 @@ def setup():
         seed = ""
     else:
         seed = q
-    q = inputquestion("Description",desc="")
+    q = inputquestion("Description",desc="Leave blank for server name")
     if q == "":
-        motd = ""
+        motd = servername
     else:
         motd = q
     info("Rewriteing Server Properties")
@@ -344,22 +385,41 @@ def setup():
     input()
     info("Please wait writing settings.json")
     f = open("settings.json","w")
-    settingsdata = {"color":1,"directory":directory,"mcversion":minecraftserverver,"mcbuild":minecraftserverver,"installerverison":installerversion}
+    settingsdata = {"servername":servername,"color":1,"directory":directory}
     json.dump(settingsdata,f,indent=4)
     f.close()
     ok("Success")
     ok("The Setup process has been completed")
     questionnogui("Are you sure that you want to start server now or head to main menu (Y/n)")
     if pyinputplus.inputYesNo() == "yes":
-        info("Starting server")
-        startserver()
+        info("Starting server...")
+        startserver(showserver=True)
+        sys.exit(0)
     else:
-        info("Starting main menu")
+        info("Starting main menu...")
         startmenu()
 
-def startmenu():
+def finderrors():
     pass
 
+def startmenu():
+    d = menu(settings["servername"],desc="Use arrow keys to navagate",["Start Server","Server settings","Installer settings","exit"])
+    if d == 1:
+        output = startserver()
+        finderrors(output)
+    elif d == 2:
+        serversettings()
+    elif d == 3:
+        sys.exit()
+    else:
+        pass
+ 
+def unpack_settings():
+    f = open("settings.json","r")
+    return json.load(f)
+
+def update_settings():
+    f = open("")
 
 if __name__ == "__main__":
     info("This Minecraft server installer is made my @mas6y6 on github")
@@ -378,7 +438,10 @@ if __name__ == "__main__":
             else:
                 fatul("Process stopped")
                 sys.exit()
-        startmenu()
+        else:
+            info("Unpacking settings.json")
+            settings = unpack_settings()
+            startmenu()
             
     else:
         error("The settings.json file was not found")
